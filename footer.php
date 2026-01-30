4672ff99ff8b5f303904b662590a12697e9726df
nakataku2010 <taku@atmos.blue>
2026-01-30 21:30:16 +0900

    ヘッダー、フッター調整中

--- footer.php
+++ footer.php
@@ -1,14 +1,39 @@
 <?php
+
 /**
  * フッター
  */
 
-if ( !defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 ?>
 
 <footer role="contentinfo" class="site-footer">
+	<div class="footer__inner">
+		<h2 class="site-branding">
+			<a href="<?php echo home_url(); ?>/" class="site-branding__link">
+				<img src="<?php echo IMG; ?>/logo.png" alt="<?php bloginfo('name'); ?>">
+			</a>
+		</h2>
+		<ul class="footer-navigation-lists">
+			<li class="footer-navigation-lists__item">
+				<a href="<?php echo home_url(); ?>/" class="footer-navigation-lists__title">トップ</a>
+			</li>
+			<li class="footer-navigation-lists__item">
+				<a href="<?php echo home_url(); ?>/schedule" class="footer-navigation-lists__title">交流会日程</a>
+			</li>
+			<li class="footer-navigation-lists__item">
+				<a href="<?php echo home_url(); ?>/about" class="footer-navigation-lists__title">初参加ガイド</a>
+			</li>
+			<li class="footer-navigation-lists__item">
+				<a href="<?php echo home_url(); ?>/blog" class="footer-navigation-lists__title">交流会ブログ</a>
+			</li>
+			<li class="footer-navigation-lists__item">
+				<a href="<?php echo home_url(); ?>/about#contact" class="footer-navigation-lists__title">お問い合わせ</a>
+			</li>
+		</ul>
+	</div>
 	<p class="copyright">
 		<small>Copyright © LEPANE All Rights Reserved.</small>
 	</p>
@@ -19,4 +44,5 @@ if ( !defined( 'ABSPATH' ) ) {
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * ヘッダー
  */
 
-if ( !defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 $first_visit = !isset($_COOKIE['visited']);
@@ -11,36 +12,70 @@ $first_visit = !isset($_COOKIE['visited']);
 
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width">
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width">
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+	<?php wp_body_open(); ?>
 
-<div class="wrapper">
-	<header role="banner" class="site-header" id="js-header">
-		<?php
-			if ( is_front_page() ) {
-		?>
-			<h1 class="site-branding">
-				<a href="<?php echo home_url(); ?>/" class="site-branding__link">
-					<img src="<?php echo IMG; ?>/logo.png" alt="<?php bloginfo('name'); ?>">
-				</a>
-			</h1>
-		<?php
-			} else {
-		?>
-			<p class="site-branding">
-				<a href="<?php echo home_url(); ?>/" class="site-branding__link">
-					<img src="<?php echo IMG; ?>/logo.png" alt="<?php bloginfo('name'); ?>">
-				</a>
-			</p>
-		<?php
-			}
-		?>
+	<div class="wrapper">
+		<header role="banner" class="site-header" id="js-header">
+			<h1 class="header__caption">異業種交流会レパンは人脈作りから成果を3倍にする名刺交換会。年間4000名が参加、1回の平均は約20名（半数以上が初参加です）東京23区内・福岡・大阪・名古屋・静岡・富山・横浜・船橋・群馬・仙台・札幌で開催中。毎回半数以上が初参加！</h1>
+			<div class="header__inner">
+				<div class="heder__logo-area">
+					<?php
+					if (is_front_page()) {
+					?>
+						<h2 class="site-branding">
+							<a href="<?php echo home_url(); ?>/" class="site-branding__link">
+								<img src="<?php echo IMG; ?>/logo.png" alt="<?php bloginfo('name'); ?>">
+							</a>
+						</h2>
+					<?php
+					} else {
+					?>
+						<p class="site-branding">
+							<a href="<?php echo home_url(); ?>/" class="site-branding__link">
+								<img src="<?php echo IMG; ?>/logo.png" alt="<?php bloginfo('name'); ?>">
+							</a>
+						</p>
+					<?php
+					}
+					?>
+				</div>
+				<nav class="site-navigation" role="navigation">
+					<ul class="site-navigation-lists">
+						<li class="site-navigation-lists__item">
+							<a href="<?php echo home_url(); ?>/" class="site-navigation-lists__title">トップ</a>
+						</li>
+						<li class="site-navigation-lists__item">
+							<a href="<?php echo home_url(); ?>/schedule" class="site-navigation-lists__title">交流会日程</a>
+						</li>
+						<li class="site-navigation-lists__item">
+							<a href="<?php echo home_url(); ?>/about" class="site-navigation-lists__title">初参加ガイド</a>
+						</li>
+						<li class="site-navigation-lists__item">
+							<a href="<?php echo home_url(); ?>/blog" class="site-navigation-lists__title">交流会ブログ</a>
+						</li>
+						<li class="site-navigation-lists__item">
+							<a href="<?php echo home_url(); ?>/about#contact" class="site-navigation-lists__title">お問い合わせ</a>
+						</li>
+					</ul>
+				</nav>
+				<div class="toggle_btn">
+					<div class="openbtn-area">
+						<span></span>
+						<span></span>
+						<span></span>
+					</div>
+				</div>
+				<!-- toggle_btn -->
+			</div>
 
-		<?php get_template_part('template', 'parts/navigation/site-navigation'); ?>
-	</header>
+			<?php //get_template_part('template', 'parts/navigation/site-navigation'); 
+			?>
+		</header>
