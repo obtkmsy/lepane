dc47b61a9588a18af411cef8caa7b88bab194270
nakataku2010 <taku@atmos.blue>
2026-02-03 09:37:16 +0900

    諸々調整

--- index.php
+++ index.php
@@ -15,14 +15,15 @@ if (!defined('ABSPATH')) {
 
 <main role="main" class="site-main">
 	<div class="wp-block-post-content has-global-padding is-layout-constrained wp-block-post-content-is-layout-constrained">
-		<?php if ( have_posts() ): ?>
+		<?php if (have_posts()): ?>
 			<div class="entry-loop has-border-top">
-				<?php while ( have_posts() ): the_post(); ?>
+				<?php while (have_posts()): the_post(); ?>
 					<?php get_template_part('template', 'parts/loop/entry-item'); ?>
 				<?php endwhile; ?>
 			</div>
 
 			<?php get_template_part('template', 'parts/component/pager'); ?>
+
 
 		<?php else: endif; ?>
 	</div>
--- page.php
+++ page.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * 固定ページ
  */
 
-if ( !defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
@@ -12,11 +13,15 @@ if ( !defined( 'ABSPATH' ) ) {
 <?php get_header(); ?>
 <main role="main" class="site-main">
 	<div class="wp-block-post-content has-global-padding is-layout-constrained wp-block-post-content-is-layout-constrained">
-		<?php if ( have_posts() ): ?>
-			<?php while ( have_posts() ): the_post(); ?>
+		<?php if (have_posts()): ?>
+			<?php while (have_posts()): the_post(); ?>
 				<?php the_content(); ?>
 			<?php endwhile; ?>
 		<?php else: endif; ?>
 	</div>
+	<?php if ((is_home() || is_front_page()) && !is_paged()) : ?>
+		<canvas id="chart01"></canvas>
+		<canvas id="chart02"></canvas>
+	<?php endif; ?>
 </main>
 <?php get_footer(); ?>
