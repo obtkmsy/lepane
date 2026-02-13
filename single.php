f83c97825ce8baf2efe99d6951080482441471ff
nakataku2010 <taku@atmos.blue>
2026-02-13 11:24:54 +0900

    single ページネーション削除

--- single.php
+++ single.php
@@ -46,7 +46,8 @@ if (!defined('ABSPATH')) {
 					</div>
 				<?php endwhile; ?>
 			<?php else: endif; ?>
-			<?php get_template_part('template', 'parts/entry/local-navigation'); ?>
+			<?php //get_template_part('template', 'parts/entry/local-navigation'); 
+			?>
 		</article>
 	</div>
 </main>
